e1a319b364a7f149460797ebe343036e56366b21
王子豪 <654026726@qq.com>
2023-09-24 18:24:03 +0800

    导航栏样式，弃用tailwindcss

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
-    <nav>
-      导航栏
+    <nav class="navlayout">
+      <h1 class="title">Nuxt3 in Action</h1>
       <div class="avatar_layout" >
-        <img  class="avatar" src="~/assets/author.jpeg" alt="avatar"/>
+        <img class="avatar" src="~/assets/author.jpeg" alt="avatar"/>
       </div>
     </nav>
     <slot />
   </div>
 </template>
-<style scoped>
-.avatar_layout {
-  display: inline-block;
-  overflow:hidden;
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  border: 1px solid rgb(218,218,218);
-}
-  .avatar {
-    height: 100%;
+<style lang="scss" scoped>
+  .navlayout {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    border-bottom: 1px solid #ccc;
+    .title {
+      font-size: 24px;
+      font-weight: 700;
+    }
+  .avatar_layout {
+    display: inline-block;
+    overflow:hidden;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+      .avatar {
+        height: 100%;
+      }
+    }
   }
 </style>
