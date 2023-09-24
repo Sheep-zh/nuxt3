038657ee0879a575d7379f548a6b419a165c3eca
王子豪 <654026726@qq.com>
2023-09-24 17:31:12 +0800

    feat: 静态资源

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <nav>导航栏</nav>
+    <nav>
+      导航栏
+      <div class="avatar_layout" >
+        <img  class="avatar" src="~/assets/author.jpeg" alt="avatar"/>
+      </div>
+    </nav>
     <slot />
   </div>
 </template>
+<style scoped>
+.avatar_layout {
+  display: inline-block;
+  overflow:hidden;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  border: 1px solid rgb(218,218,218);
+}
+  .avatar {
+    height: 100%;
+  }
+</style>
