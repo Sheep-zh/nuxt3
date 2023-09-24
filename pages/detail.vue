9a37d04b3ad79ed9d4347c10c4f6131fd384bccb
王子豪 <654026726@qq.com>
2023-09-24 17:11:33 +0800

    feat: 嵌套路由

---
+++ pages/detail.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>
+    detail no id
+  </h1>
+  <NuxtPage></NuxtPage>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <div>
     index page
     <br/>
-    <NuxtLink to="/detail-1">detail-1</NuxtLink>
+    <NuxtLink to="/detail/1">detail-1</NuxtLink>
     <br/>
-    <NuxtLink to="/detail-2">detail-2</NuxtLink>
+    <NuxtLink to="/detail/2">detail-2</NuxtLink>
     <br/>
-    <NuxtLink to="/detail-3">detail-3</NuxtLink>
+    <NuxtLink to="/detail/3">detail-3</NuxtLink>
   </div>
 </template>
